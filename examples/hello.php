63bba8748648de435d49a6d879678311a0340140
Leon Renkema <leonrenkema@gmail.com>
2014-08-16 18:18:13 +0200

    Added vendor to ignore

--- examples/hello.php
+++ examples/hello.php
@@ -9,7 +9,7 @@ $jetfile = new JetFile2();
 $t = new Tcp('192.168.178.22');
 
 
-$cmd = $jetfile->getTextCommand('{center}{p5}{s3}{bgblack}{amber}{%d-%m-%C}{nf}{%H}:{%M}:{%S}{nf}{red}test');
+$cmd = $jetfile->getTextCommand('{center}{p5}{s5}{bgblack}{amber}{%d-%m-%C}{nf}{%H}:{%M}:{%S}{nf}{red}test');
 date_default_timezone_set('CET');
 $t->sendCommand($cmd);
 
